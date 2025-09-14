4a2562f252e8b97ea7ccc62430a50086ebf879ca
Zachariah <spencerzachariah@gmail.com>
2025-09-14 15:23:18 -0500

    add email notifications and stock notifs

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -44,6 +44,6 @@ class ProductsController < ApplicationController
   end
 
   def product_params
-    params.expect(product: [ :name, :description, :featured_image ])
+    params.expect(product: [ :name, :description, :featured_image, :inventory_count ])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -27,6 +27,10 @@ Rails.application.routes.draw do
   #
   # delete "/products/:id", to: "products#destroy"
 
-  resources :products
+  resources :products do
+    resources :subscribers, only: [ :create ]
+  end
+
+  resource :unsubscribe, only: [ :show ]
   root "products#index"
 end
